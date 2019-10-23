7c740bad6829609e2ae72bedb5a3a4fbd95c0ff0
Eric Mac <ericmackensen@icloud.com>
2019-10-23 01:08:36 -0700

    picker + button (dynamically rendered) from api data and base style (space around)

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,48 +1,83 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button, Picker } from 'react-native'
 import allCities from '../api/allCities'
 
 class HomeScreen extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            cityLocations: []
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      cityLocations: [],
+      pickerLocation: ''
     }
+  }
 
-    static navigationOptions = {
-        title: 'HomeScreen',
-      }
+  static navigationOptions = {
+    title: 'HomeScreen',
+  }
 
-    getAllCities = async () => {
-        const response = await allCities.get('/')
-        this.setState({
-          cityLocations: response.data
-        })
-        // console.log(response.data, 'Response')
-        console.log('City Locations', this.state.cityLocations)
-      }
+  getAllCities = async () => {
+    const response = await allCities.get('/')
+    // picker location sets initial picker selector to the first city in the data set for display on button
+    this.setState({
+      cityLocations: response.data,
+      pickerLocation: response.data[0].name
+    })
+    console.log('City Locations', this.state.cityLocations)
+  }
 
-      componentDidMount() {
-          console.log('comp did mount')
-        this.getAllCities()
-      }
+  componentDidMount() {
+    console.log('comp did mount')
+    this.getAllCities()
+  }
 
-    render() {
-        const {navigate} = this.props.navigation
-        return (
-            <View>
-                <Text>HomeScreen</Text>
-                <Button
-                title="Map View"
-                onPress={() => this.props.navigation.navigate('MapView')}
-                />
-            </View>
-        )
-    }
+  render() {
+    const { navigate } = this.props.navigation
+    return (
+      <View style={styles.wrapperView}>
+
+        <View style={{ flex: 3, justifyContent: "space-around" }}>
+          <Button
+            title="Current Location"
+            onPress={() => navigate('MapView')}
+          />
+        </View>
+
+        <View style={{ flex: 3, justifyContent: "space-around", alignItems: "center"}}>
+          <Text>or</Text>
+        </View>
+
+        <View style={{ flex: 3, justifyContent: "space-around" }}>
+          <Picker
+            selectedValue={this.state.language}
+            // callback changes the value of state for dynamic button
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({ pickerLocation: itemValue })
+            }>
+            {this.state.cityLocations.map((element) => {
+              return (
+                <Picker.Item label={element.name} value={element.name} key={element.name} />
+              )
+            })}
+          </Picker>
+        </View>
+
+        <View style={{ flex: 3, justifyContent: "space-around" }}>
+          <Button
+            title={`Go to ${this.state.pickerLocation}`}
+            onPress={() => navigate('MapView')}
+          />
+        </View>
+
+      </View>
+    )
+  }
 
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  wrapperView: {
+    flex: 1
+  }
+})
 
 export default HomeScreen
