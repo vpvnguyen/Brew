5ff076adf17934ca90198fd649351c7a3a5c6683
Eric Mac <ericmackensen@icloud.com>
2019-10-29 16:55:28 -0700

    geo location, fix button disable

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,7 +6,8 @@ class HomeScreen extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            geoLocation: undefined
+            location: undefined,
+            loading: undefined
         }
     }
     static navigationOptions = ({ navigation }) => {
@@ -35,20 +36,56 @@ class HomeScreen extends Component {
     //     }
     //   }
 
+    hasLocationPermission = async () => {
+        if (Platform.OS === 'ios' ||
+            (Platform.OS === 'android' && Platform.Version < 23)) {
+          return true;
+        }
+    
+        const hasPermission = await PermissionsAndroid.check(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+        );
+    
+        if (hasPermission) return true;
+    
+        const status = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
+        );
+    
+        if (status === PermissionsAndroid.RESULTS.GRANTED) return true;
+    
+        if (status === PermissionsAndroid.RESULTS.DENIED) {
+          ToastAndroid.show('Location permission denied by user.', ToastAndroid.LONG);
+        } else if (status === PermissionsAndroid.RESULTS.NEVER_ASK_AGAIN) {
+          ToastAndroid.show('Location permission revoked by user.', ToastAndroid.LONG);
+        }
+    
+        return false;
+      }
+
+    // Instead of navigator.geolocation, just use Geolocation.
+    getLocation = async () => {
+        const hasLocationPermission = await this.hasLocationPermission();
+    
+        if (!hasLocationPermission) return;
+    
+        this.setState({ loading: true }, () => {
+          Geolocation.getCurrentPosition(
+            (position) => {
+              this.setState({ location: position, loading: false });
+              console.log(position);
+            },
+            (error) => {
+              this.setState({ location: error, loading: false });
+              console.log(error);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000, distanceFilter: 50, forceRequestLocation: true }
+          );
+        });
+      }
+
     componentDidMount() {
-        // Instead of navigator.geolocation, just use Geolocation.
-        // if (hasLocationPermission) {
-            Geolocation.getCurrentPosition(
-                (position) => {
-                    console.log(position, "geolocation coords");
-                },
-                (error) => {
-                    // See error code charts below.
-                    console.log(error.code, error.message);
-                },
-                { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-            );
-        // }
+        this.getLocation()
     }
 
     render() {
@@ -70,7 +107,7 @@ class HomeScreen extends Component {
                         title={`Or Use Current Location`}
                         onPress={() => navigate('MapScreen')}
                         // if userlocation hasnt been changed from its default undfined value button is disabaled
-                        disabled={this.state.userLocation === undefined}
+                        disabled={this.state.userLocation !== undefined}
                     />
                 </View>
             </View>
