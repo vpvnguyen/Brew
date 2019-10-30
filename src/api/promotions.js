ca6edf22564d73906fa8d80cc80f0cc27356ecb6
Eric Mac <ericmackensen@icloud.com>
2019-10-29 19:12:43 -0700

    api routes

--- src/api/promotions.js
+++ src/api/promotions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:5000/api/promotion/all'
+    baseURL: 'https://quiet-lake-29855.herokuapp.com/api/promotion/all'
 });
